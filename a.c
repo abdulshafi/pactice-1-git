70a3e11f316c2306276e676f209fd80b907ab7d3
Abdulshafi AL Whshy <abdulshafi1999727@gmail.com>
2018-05-01 16:28:16 +0000

    asdsdq

--- a.c
+++ a.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
-void swap(int x, int z)
+void swap(int *x, int *z)
 {
-    int a = z; z = x; x = a;
+    int a = *z; *z = *x; *x = a;
 }
 int main(void)
 {
     int x = 0, z = 1;
 
-    swap(x, z);
+    swap(&x, &z);
+
 }
